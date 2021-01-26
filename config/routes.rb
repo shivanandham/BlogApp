0f44b41cf8c4f067239e23290aa2fde5f151b51c
shiva <rootofshiva@gmail.com>
2021-01-26 20:03:42 +0530

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_up: 'register', sign_out: 'logout'}
+  resources :blogs
+  root to: "blogs#index"
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+end
