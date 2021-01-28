739b8efa3a5897fd4f60d5bb16a28a9924f72d7a
shiva <rootofshiva@gmail.com>
2021-01-28 11:22:38 +0530

    edited permissions for comments params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
 	before_action :authenticate_user!
 	def create
 		@blog_post = BlogPost.find(params[:blog_post_id])
-		@comment = @blog_post.comments.create(params[:comment].permit(:name, :comment))
+		@comment = @blog_post.comments.create(params[:comment].permit(:comment))
 		@comment.email = current_user.email
     	@comment.save!
 		redirect_to blog_post_path(@blog_post)
