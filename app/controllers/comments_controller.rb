1c056c928ddedc461f28bafdd3821a7f1f9da695
shiva <rootofshiva@gmail.com>
2021-01-29 00:01:45 +0530

    finally got it working and iam stupid

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,9 +4,9 @@ class CommentsController < ApplicationController
 		@blog_post = BlogPost.find(params[:blog_post_id])
 		@user = User.find(current_user.id)		
 		@comment = @blog_post.comments.new(params[:comment].permit(:comment))
-		@comment_user = @user.comments.new(params[:comment].permit(:comment))
+		@comment.user_id = @user.id
 		@comment.save!
-		@comment_user.save!
+		#@comment_user.save!
 		redirect_to blog_post_path(@blog_post)
 	end
 end
